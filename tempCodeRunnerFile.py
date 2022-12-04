d6694a7e32e6ad6d2f86ddcee78de3c4e9965b58
Vishnu9535 <vk9255888@gmail.com>
2022-12-04 11:45:43 +0530

    ...

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+
+import tensorflow as tf
+from tensorflow import keras
+from keras.datasets import mnist    
+import pandas as pd
+import numpy as np  
+import csv
+dataset='A_ZHandwrittenData.csv' 
+# for row in dataset:
+#     row=row.split(",")
+
+#     image=np.array([int(i) for i in row[1:]],dtype="uint8")
+#     print(image)import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
