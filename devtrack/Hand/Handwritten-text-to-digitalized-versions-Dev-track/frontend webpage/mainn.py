c295fd251042fb7797ff97c919b23ccb7999c60b
Vishnu9535 <vk9255888@gmail.com>
2023-01-29 17:01:56 +0530

    ..

--- devtrack/Hand/Handwritten-text-to-digitalized-versions-Dev-track/frontend webpage/mainn.py
+++ devtrack/Hand/Handwritten-text-to-digitalized-versions-Dev-track/frontend webpage/mainn.py
@@ -19,7 +19,7 @@ def home():
     form = UploadFileForm()
     if form.validate_on_submit():
         file = form.file.data # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(filaibhave.filename))) # Then save the file
         
     return render_template('index.html', form=form)
 
